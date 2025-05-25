25102e3357360b87924e702a93a5b75a31fa3a43
Huan Zhou <me@per1cycle.org>
2025-05-25 23:10:34 +0800

    save.

--- opt/main.cc
+++ opt/main.cc
@@ -3,6 +3,19 @@
 
 int main()
 {
+	constexpr uint M = 1024, N = 1024, K = 1024;
+	
+	float *a = new float[M * K];
+	float *b = new float[K * N];
+	float *c = new float[M * N];
+	
+	utils::generate_T_matrix(a, M, K);
+	utils::generate_T_matrix(a, M, K);
+
+
+	delete []a;
+	delete []b;
+	delete []c;
 
 	return 0;
 }
